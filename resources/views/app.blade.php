7b7d12b3632512b0ef20621e0753471caf7f1daa
Jan <mail@jan-claar.de>
2022-05-25 11:44:58 +0200

    Start Inertia

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+        <title>Laravel</title>
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+        @inertiaHead
+    </head>
+    <body>
+        @inertia
+    </body>
+</html>
